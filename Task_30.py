fb45d1d6f43f7dd7ded6eb3f7d74dca62c2665cd
Konstantin <constantin8271@gmail.com>
2022-05-12 20:28:05 +0300

    task_30.fix

--- Task_30.py
+++ Task_30.py
@@ -10,3 +10,12 @@ d = randint(1, 10)
 print('Точность вывода = ', d)
 
 print('Пи = ', round(pi, d), '\n')
+
+exit()
+import math
+P = 0.001
+#P = float(input("Введите количество знаков после запятой для вычисления числа Пи (от 1 до 10 ):\n"))
+P = str(P).split('.')
+L = len(P[1])
+M = str(math.pi)
+print(float(M[:L+2]))
